d71f28930807672f46fc4f0b415484aa9ec89714
philippe gabriel <philippe.gabriel@citrix.com>
2015-10-30 16:37:44 +0000

    fixed timestamp format

--- changeditemHistory.sql
+++ changeditemHistory.sql
@@ -1,4 +1,4 @@
-select cg.author,'changed',ci.field,'of',concat(p.pkey,'-',j.issuenum),'from',ci.oldstring,'to',ci.newstring,'at',to_char(cg.created, 'hh:mm:ss')
+select cg.author,'changed',ci.field,'of',concat(p.pkey,'-',j.issuenum),'from',ci.oldstring,'to',ci.newstring,'at',to_char(cg.created, 'HH24:MI:SS')
 from changegroup cg
 inner join changeitem ci on ci.groupid=cg.id
 inner join jiraissue j	on cg.issueid=j.id
--- commentsHistory.sql
+++ commentsHistory.sql
@@ -1,6 +1,6 @@
 -- list jira issues where 'NAME', acted on a jira ticket since a given interval 
 -- Using the jira SQL interface, see: https://developer.atlassian.com/display/JIRADEV/Database+Schema
-select a.author,'added a',a.actiontype,'to',concat(p.pkey,'-',j.issuenum),'at',to_char(a.created, 'hh:mm:ss')
+select a.author,'added a',a.actiontype,'to',concat(p.pkey,'-',j.issuenum),'at',to_char(a.created,'HH24:MI:SS')
 from jiraaction a, jiraissue j, project p
 where a.created >= NOW() - :since::INTERVAL 
 and a.issueid=j.id 
